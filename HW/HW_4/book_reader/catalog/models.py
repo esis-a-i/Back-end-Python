bb50b2fce6c0b1adc296a20cce2d2ff8f0753059
AlexEsn <saschaesn@gmail.com>
2021-12-06 17:59:19 +0300

    HW 5

--- HW/HW_4/book_reader/catalog/models.py
+++ HW/HW_4/book_reader/catalog/models.py
@@ -2,6 +2,24 @@ from django.db import models
 from django.urls import reverse
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Title of book")
+
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of the book", verbose_name="Summary", null=True, blank=True)
+
+    genre = models.ManyToManyField(
+        'Genre', help_text="Select a genre for this book")
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse('book-detail', args=[str(self.id)])
+
+
 class Genre(models.Model):
     name = models.CharField(
         max_length=200, help_text="Enter a book genre", verbose_name="Genre")
@@ -9,38 +27,23 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse("genre_detail", args=[str(self.id)])
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Title of book")
-
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-
-    summary = models.TextField(
-        max_length=1000, help_text="Enter a brief description of the book", verbose_name="Summary")
-
-    genre = models.ManyToManyField(
-        Genre, help_text="Select a genre for this book")
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('book-detail', args=[str(self.id)])
+    # def get_absolute_url(self):
+    #     return reverse("genre_detail", args=[str(self.id)])
 
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100, verbose_name="First name")
+
     last_name = models.CharField(max_length=100, verbose_name="Last name")
+
     date_of_birth = models.DateField(
         null=True, blank=True, verbose_name="Date of birth")
+
     date_of_death = models.DateField(
         null=True, blank=True, verbose_name="Date of death")
 
-    def get_absolute_url(self):
-        return reverse('author-detail', args=[str(self.id)])
-
     def __str__(self):
-        return '%s, %s' % (self.last_name, self.first_name)
+        return f'{self.last_name}, {self.first_name}'
+
+    # def get_absolute_url(self):
+    #     return reverse('author-detail', args=[str(self.id)])
